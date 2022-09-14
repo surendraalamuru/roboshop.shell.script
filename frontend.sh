745ddece8dc92d79f7c750b78484f79f5b5444cc
surendra reddy <surendrareddylamuru01@gmail.com>
2022-09-14 16:04:02 +0530

    Adding status

--- frontend.sh
+++ frontend.sh
@@ -1,17 +1,20 @@
 echo installing Nginx
 yum install nginx -y
+echo status = $?
 
 echo Downloading Nginx web content  &>>/tmp/frontend
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>/tmp/frontend
+echo status = $?
 
 cd /usr/share/nginx/html
 
 echo Removing web content
 rm -rf * &>>/tmp/frontend
+echo status = $?
 
 echo Extracting web content
 unzip /tmp/frontend.zip &>>/tmp/frontend
-
+echo status = $?
 
 mv frontend-main/static/* . &>>/tmp/frontend
 mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf &>>/tmp/frontend
@@ -19,3 +22,4 @@ mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf &>>/tmp/front
 echo Starting Nginx Service
 systemctl enable nginx &>>/tmp/frontend
 systemctl restart nginx &>>/tmp/frontend
+echo status = $?
