8731b560734179911e1e8b9d27c31b09e52b8444
surendra reddy <surendrareddylamuru01@gmail.com>
2022-09-20 09:31:21 +0530

    common.sh

--- common.sh
+++ common.sh
@@ -52,7 +52,7 @@ echo "Install NodeJS Dependencies"
 npm install &>>${LOG_FILE}
 StatusCheck $?
 echo "Update System Service File"
-sed -i -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/MONGO_ENDPOINT/mongodb.roboshop.internsal/' -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/' -e 's/MONGO_DNSNAME/mongodb.roboshop.internal' /home/roboshop/${COMPONENT}/systemd.service
+sed -i -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/MONGO_ENDPOINT/mongodb.roboshop.internsal/' -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/' -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' /home/roboshop/${COMPONENT}/systemd.service
 StatusCheck $?
 
 echo "Setup ${COMPONENT} Service"
